32faa9aee95b659b3ef35da54da4a91526adac91
Lassi Kortela <lassi@lassi.io>
2019-12-27 22:55:04 +0200

    Add test framework

---
+++ framework.scm
@@ -0,0 +1,6 @@
+(define-syntax check-equal?
+  (syntax-rules (value)
+    ((_ expr expected-value)
+     (let ((value expr))
+       (or (equal? value expected-value)
+           (error "Test failed: not equal" 'expr value expected-value))))))
---
+++ framework.sld
@@ -0,0 +1,4 @@
+(define-library (srfi-test framework)
+  (export check-equal?)
+  (import (scheme base))
+  (include "framework.scm"))
