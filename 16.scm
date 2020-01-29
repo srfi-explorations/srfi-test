273a457dea0b4a4d3528a86087532fc999394961
Lassi Kortela <lassi@lassi.io>
2020-01-29 22:25:11 +0200

    Re-indent

--- 16.scm
+++ 16.scm
@@ -1,38 +1,38 @@
-      (define plus
-        (case-lambda
-         (() 0)
-         ((x) x)
-         ((x y) (+ x y))
-         ((x y z) (+ (+ x y) z))
-         (args (apply + args))))
-      (define print
-        (case-lambda
-         (()
-          (display ""))
-         ((arg)
-          (display arg))
-         ((arg . args)
-          (display arg)
-          (display " ")
-          (apply print args))))
-      (define (print-to-string . args)
-        (let ((out (open-output-string))
-              (old-out (current-output-port)))
-          (dynamic-wind
-            (lambda () (current-output-port out))
-            (lambda () (apply print args))
-            (lambda () (current-output-port old-out)))
-          (get-output-string out)))
+(define plus
+  (case-lambda
+    (() 0)
+    ((x) x)
+    ((x y) (+ x y))
+    ((x y z) (+ (+ x y) z))
+    (args (apply + args))))
+(define print
+  (case-lambda
+    (()
+     (display ""))
+    ((arg)
+     (display arg))
+    ((arg . args)
+     (display arg)
+     (display " ")
+     (apply print args))))
+(define (print-to-string . args)
+  (let ((out (open-output-string))
+        (old-out (current-output-port)))
+    (dynamic-wind
+      (lambda () (current-output-port out))
+      (lambda () (apply print args))
+      (lambda () (current-output-port old-out)))
+    (get-output-string out)))
 
-      (test-begin "srfi-16")
+(test-begin "srfi-16")
 
-      (test-eqv 0 (plus))
-      (test-eqv 1 (plus 1))
-      (test-eqv 6 (plus 1 2 3))
-      (test-error ((case-lambda ((a) a) ((a b) (* a b))) 1 2 3))
+(test-eqv 0 (plus))
+(test-eqv 1 (plus 1))
+(test-eqv 6 (plus 1 2 3))
+(test-error ((case-lambda ((a) a) ((a b) (* a b))) 1 2 3))
 
-      (test-equal "" (print-to-string))
-      (test-equal "hi" (print-to-string 'hi))
-      (test-equal "hi there world" (print-to-string 'hi 'there 'world))
+(test-equal "" (print-to-string))
+(test-equal "hi" (print-to-string 'hi))
+(test-equal "hi there world" (print-to-string 'hi 'there 'world))
 
-      (test-end "srfi-16")
+(test-end "srfi-16")
