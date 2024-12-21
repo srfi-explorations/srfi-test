a5c7d9b3cc331d87c2c65a78b593d2fbb9515053
Retropikzel <retropikzel@iki.fi>
2024-12-21 15:10:41 +0200

    Add tests for srfi-19 (#6)

--- convert.scm
+++ convert.scm
@@ -116,7 +116,8 @@
     (scheme char)
     (scheme write)
     (scheme process-context)
-    (scheme file)))
+    (scheme file)
+    (scheme cxr)))
 
 (define (srfi-import-numbers srfi-number . extra-srfi-numbers)
   (append (list srfi-number)
@@ -231,7 +232,7 @@
 ;;
 
 (define all-srfis
-  '(1 2 8 11 13 14 16 26 28 33 39 64 60 69 115 129 130 132 133 151 160 175))
+  '(1 2 8 11 13 14 16 19 26 28 33 39 64 60 69 115 129 130 132 133 151 160 175))
 
 (for-each write-r7rs-test-library all-srfis)
 (for-each write-r7rs-test-program all-srfis)
