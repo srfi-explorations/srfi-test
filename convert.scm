322a9b39c675bee5db7bd1d05362bff79970c949
retropikzel <retropikzel@iki.fi>
2025-07-18 18:10:24 +0300

    Adding more tests, fixing some

--- convert.scm
+++ convert.scm
@@ -270,7 +270,7 @@
 ;;
 
 (define all-srfis
-  '(1 2 4 5 8 11 13 14 16 19 25 26 27 28 31 37 38 39 48 64 60 69 115 129 130 132 133 151 160 175))
+  '(1 2 4 5 8 11 13 14 16 19 25 26 27 28 29 31 37 38 39 48 64 60 69 115 129 130 132 133 151 160 175))
 
 (for-each write-r7rs-test-library all-srfis)
 (for-each write-r7rs-test-program all-srfis)
