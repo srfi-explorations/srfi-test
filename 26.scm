baff3bbab9f60d85b34a59bac6e078042cbd0d37
retropikzel <retropikzel@iki.fi>
2025-07-18 06:34:13 +0300

    Change syntax from using [] to using ()

--- 26.scm
+++ 26.scm
@@ -16,9 +16,11 @@
 (test-equal "cut list 1 <> 3 <...>"
   '(1 2 3 4 5 6) ((cut list 1 <> 3 <...>) 2 4 5 6))
 (test-equal "cut (eval order)" '(ok)
-       (let* ([x 'wrong] [y (cut list x)]) (set! x 'ok) (y)))
+       (let* ((x 'wrong)
+              (y (cut list x)))
+         (set! x 'ok) (y)))
 (test-equal "cut (eval order)" 2
-       (let ([a 0])
+       (let ((a 0))
          (map (cut + (begin (set! a (+ a 1)) a) <>)
               '(1 2))
          a))
@@ -36,9 +38,11 @@
 (test-equal "cute list 1 <> 3 <...>"
   '(1 2 3 4 5 6) ((cute list 1 <> 3 <...>) 2 4 5 6))
 (test-equal "cute (eval order)" '(ok)
-       (let* ([x 'ok] [y (cute list x)]) (set! x 'wrong) (y)))
+       (let* ((x 'ok)
+              (y (cute list x)))
+         (set! x 'wrong) (y)))
 (test-equal "cute (eval order)" 1
-       (let ([a 0])
+       (let ((a 0))
          (map (cute + (begin (set! a (+ a 1)) a) <>)
               '(1 2))
          a))
