f7c4a53147a653ef7a9c248aa571b00000034982
retropikzel <retropikzel@iki.fi>
2025-08-23 12:13:04 +0300

    Fixing tests for srfi-4

--- convert.scm
+++ convert.scm
@@ -117,16 +117,6 @@
 
 (define (r7rs-imports srfi-number)
   (cond
-    ((= srfi-number 4)
-     '((except (scheme base)
-               bytevector-copy
-               bytevector-copy!)
-       (scheme char)
-       (scheme write)
-       (scheme file)
-       (scheme process-context)
-       (rename (r6rs bytevectors)
-               (bytevector-copy! r6rs:bytevector-copy!))))
     ((= srfi-number 13)
      '((except (scheme base)
                string-copy
