70ce10bd33b0be0e14723b9556c884a30ff31ed6
retropikzel <retropikzel@iki.fi>
2025-08-26 20:31:59 +0300

    Fixes for srfi-4 test

--- convert.scm
+++ convert.scm
@@ -118,7 +118,9 @@
 (define (r7rs-imports srfi-number)
   (cond
     ((= srfi-number 4)
-     '((scheme base)
+     '((except (scheme base)
+               bytevector-copy
+               bytevector-copy!)
        (scheme char)
        (scheme write)
        (scheme file)
