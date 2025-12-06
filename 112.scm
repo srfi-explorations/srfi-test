2f21c1564f49ad783bfbe23ef8b0f87a2e9a013a
retropikzel <retropikzel@iki.fi>
2025-12-06 14:21:52 +0200

    Add SRFI-112

---
+++ 112.scm
@@ -0,0 +1,18 @@
+
+(test-begin "srfi-112")
+
+(write (implementation-name))
+(newline)
+(write (implementation-version))
+(newline)
+(write (cpu-architecture))
+(newline)
+(write (machine-name))
+(newline)
+(write (os-name))
+(newline)
+(write (os-version))
+(newline)
+
+
+(test-end "srfi-112")
