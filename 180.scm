5cff17b4818cab9c8ef3fe41fd343d47acf50659
retropikzel <retropikzel@iki.fi>
2026-03-07 20:55:05 +0200

    Adding tests for SRFI-180

--- 180.scm
+++ 180.scm
@@ -1,7 +1,14 @@
 
 (test-begin "srfi-180")
 
-(test-assert (json-write `((a . 1) (b . 2) (b . 3))))
+(define (json->string json)
+  (let ((port (open-output-string)))
+    (json-write json port)
+    (get-output-string port)))
+
+(test-equal "{\"a\":1,\"b\":2,\"b\":3}" (json->string `((a . 1) (b . 2) (b . 3))))
+
+(test-equal "[1,2,3,4,5,6,7,8,9]" (json->string (vector 1 2 3 4 5 6 7 8 9)))
 
 (test-end "srfi-180")
 
