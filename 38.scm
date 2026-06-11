bfb2ae282becbc7307ff6148b9378b5ec3205e6a
retropikzel <retropikzel@iki.fi>
2026-06-11 06:58:50 +0300

    Improve srfi-38 tests

--- 38.scm
+++ 38.scm
@@ -1,3 +1,26 @@
 (test-begin "srfi-38")
 
+(define (obj->string obj)
+  (parameterize
+    ((current-output-port (open-output-string)))
+    (write-with-shared-structure obj)
+    (get-output-string (current-output-port))))
+
+(define (string->obj str)
+  (read-shared-structure (open-input-string str)))
+
+(let ((a '(val1 val2)))
+  (set! a (cons a (list a)))
+  (let ((teststr (obj->string a)))
+    (test-assert "circular"
+                 (or (string=? teststr "(#0=(val1 val2 #1#)")
+                     (string=? teststr "(#1=(val1 val2) #1#)")
+                     (string=? teststr "(#2=(val1 val2) #2#)")))))
+
+(test-equal "list" (obj->string '(val1 val2)) "(val1 val2)")
+
+(test-equal "number" (obj->string 5) "5")
+
+(test-equal "string" (obj->string "Just a string") "\"Just a string\"")
+
 (test-end "srfi-38")
